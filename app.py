4e3d7a2b8294de082d982e069120a69c93f4de87
ganesmit <antionenesmith@gmail.com>
2024-05-08 06:16:06 -0400

    Part 1 Complete

---
+++ app.py
@@ -0,0 +1,93 @@
+"""Blogly application."""
+
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def root():
+    """Homepage redirects to list of users."""
+
+    return redirect("/users")
+
+
+@app.route('/users')
+def users_index():
+    """Show all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
+
+
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show an add form for users"""
+
+    return render_template('users/new.html')
+
+
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Processing the form for a new user"""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show info on a given user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show edit page for a user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Process edit form, return user to users page"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Delete the user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
---
+++ models.py
@@ -0,0 +1,26 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
+
+
+def connect_db(app):
+    """Connect database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
